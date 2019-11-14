81a8a6a53cf7061f60c4001f0178d407070ac339
ChristianZaar <christian.zaar.82@gmail.com>
2019-11-14 09:23:06 +0100

    Changing computer(broken)

--- Excersize3/MainWindow.xaml.cs
+++ Excersize3/MainWindow.xaml.cs
@@ -24,10 +24,9 @@ namespace Excersize3
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Action filterTicketsAction;
+        
         private string defultChoice = "All";
-        private Data data;
-        private List<Ticket> tickets;
+        
         private bool blockSelectionChanged = false;
         private Ticket selectedTicket;
         private Func<List<string>> GetCountriesFromList;
@@ -39,41 +38,8 @@ namespace Excersize3
         public MainWindow()
         {
             InitializeComponent();
-            data = new Data();
-            tickets = data.Tickets; 
-
-            filterTicketsAction += CountryFilter;
-            filterTicketsAction += CostumerFilter;
-            filterTicketsAction += AdminFilter;
-            filterTicketsAction += AdminOfficeFilter;
-
-            // Get selectable countries
-            GetCountriesFromList = () => tickets.Join(
-                    data.Customers, t => t.PosterId,
-                    c => c.ID,
-                    (t, c) => c.Country)
-                    .Distinct().ToList();
-
-            // Get selectable customer names
-            GetCustomerNamesFromList = () => tickets.Join(
-                    data.Customers, t => t.PosterId,
-                    c => c.ID,
-                    (t, c) => c.Name)
-                    .Distinct().ToList();
-
-            // Get selectable admin names
-            GetAdminNamesFromList = () => tickets.SelectMany(t => t.Answers)
-                    .ToList().Select(a => a.Key)
-                    .Distinct().ToList()
-                    .Join(data.Admins, i => i, a => a.ID, (i, a) => a.Name)
-                    .ToList();
-
-            // Get selectable office admin names
-            GetAdminOfficeNamesFromList = () => tickets.SelectMany(t => t.Answers)
-                    .ToList().Select(a => a.Key)
-                    .Distinct().ToList()
-                    .Join(data.Admins, i => i, a => a.ID, (i, a) => a.Office)
-                    .ToList();
+            
+            
 
             // Get selected item Combobox
             GetSelectedItem = (ComboBox cb) =>cb.SelectedItem as String;
@@ -100,89 +66,7 @@ namespace Excersize3
             PopulateListBox();
         }
 
-        /// <summary>
-        /// Filters countries
-        /// </summary>
-        private void CountryFilter()
-        {
-            string choice = GetSelectedItem(countryComboBox);
-            
-            if (choice != null && !choice.Equals(defultChoice))
-            {
-                tickets = data.Customers
-                    .Where(c => c.Country.Equals(choice))
-                    .Join(tickets, c => c.ID, t => t.PosterId, (c, t) => t)
-                    .ToList();
-            }
-        }
-
-        /// <summary>
-        /// Filters Customers
-        /// </summary>
-        private void CostumerFilter()
-        {
-            string choice = GetSelectedItem(customerComboBox);
-
-            if (choice != null && !choice.Equals(defultChoice))
-            {
-                tickets = data.Customers
-                    .Where(c => c.Name.Equals(choice))
-                    .Join(tickets, c => c.ID, t => t.PosterId, (c, t) => t)
-                    .ToList();
-            }
-        }
-
-        /// <summary>
-        /// Filter admins
-        /// </summary>
-        private void AdminFilter()
-        {
-            string choice = GetSelectedItem(adminComboBox);
-
-            if (choice != null && !choice.Equals(defultChoice))
-            {
-                Admin admin = data.Admins.Find(a => a.Name.Equals(choice));
-                List<Ticket> tmp = new List<Ticket>();
-                tickets = FindAdminAnsweredTicket(admin);
-            }
-        }
-
-        /// <summary>
-        /// Admin office filter
-        /// </summary>
-        private void AdminOfficeFilter()
-        {
-            string choice = GetSelectedItem(officeAdminComboBox);
-
-            if (choice != null && !choice.Equals(defultChoice))
-            {
-                Admin admin = data.Admins.Find(a => a.Office.Equals(choice));
-                List<Ticket> tmp = new List<Ticket>();
-                tickets = FindAdminAnsweredTicket(admin);
-            }
-        }
-
-        /// <summary>
-        /// Find ticket that is answered by admin
-        /// </summary>
-        /// <param name="admin"></param>
-        /// <returns></returns>
-        private List<Ticket> FindAdminAnsweredTicket(Admin admin)
-        {
-            List<Ticket> tmp = new List<Ticket>();
-            foreach (Ticket t in tickets)
-            {
-                foreach (KeyValuePair<int, Answer> entry in t.Answers)
-                {
-                    if (entry.Key == admin.ID)
-                    {
-                        tmp.Add(t);
-                        break;
-                    }
-                }
-            }
-            return tickets = tmp;
-        }
+        
 
         /// <summary>
         /// Set source comboBoxes
---
+++ Excersize3/TicketManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersize3
+{
+    class TicketManager
+    {
+        private Func<List<string>> GetCountriesFromList;
+        private Func<List<string>> GetCustomerNamesFromList;
+        private Func<List<string>> GetAdminNamesFromList;
+        private Func<List<string>> GetAdminOfficeNamesFromList;
+        private Data data;
+        private List<Ticket> tickets;
+        private Action filterTicketsAction;
+
+        public TicketManager()
+        {
+
+            data = new Data();
+            tickets = data.Tickets;
+
+            filterTicketsAction += CountryFilter;
+            filterTicketsAction += CostumerFilter;
+            filterTicketsAction += AdminFilter;
+            filterTicketsAction += AdminOfficeFilter;
+
+            // Get selectable countries
+            GetCountriesFromList = () => tickets.Join(
+                    data.Customers, t => t.PosterId,
+                    c => c.ID,
+                    (t, c) => c.Country)
+                    .Distinct().ToList();
+
+            // Get selectable customer names
+            GetCustomerNamesFromList = () => tickets.Join(
+                    data.Customers, t => t.PosterId,
+                    c => c.ID,
+                    (t, c) => c.Name)
+                    .Distinct().ToList();
+
+            // Get selectable admin names
+            GetAdminNamesFromList = () => tickets.SelectMany(t => t.Answers)
+                    .ToList().Select(a => a.Key)
+                    .Distinct().ToList()
+                    .Join(data.Admins, i => i, a => a.ID, (i, a) => a.Name)
+                    .ToList();
+
+            // Get selectable office admin names
+            GetAdminOfficeNamesFromList = () => tickets.SelectMany(t => t.Answers)
+                    .ToList().Select(a => a.Key)
+                    .Distinct().ToList()
+                    .Join(data.Admins, i => i, a => a.ID, (i, a) => a.Office)
+                    .ToList();
+         
+
+        }
+
+        /// <summary>
+        /// Filters countries
+        /// </summary>
+        private void CountryFilter(string choice)
+        {
+
+            if (choice != null && !choice.Equals(defultChoice))
+            {
+                tickets = data.Customers
+                    .Where(c => c.Country.Equals(choice))
+                    .Join(tickets, c => c.ID, t => t.PosterId, (c, t) => t)
+                    .ToList();
+            }
+        }
+
+        /// <summary>
+        /// Filters Customers
+        /// </summary>
+        private void CostumerFilter()
+        {
+            string choice = GetSelectedItem(customerComboBox);
+
+            if (choice != null && !choice.Equals(defultChoice))
+            {
+                tickets = data.Customers
+                    .Where(c => c.Name.Equals(choice))
+                    .Join(tickets, c => c.ID, t => t.PosterId, (c, t) => t)
+                    .ToList();
+            }
+        }
+
+        /// <summary>
+        /// Filter admins
+        /// </summary>
+        private void AdminFilter()
+        {
+            string choice = GetSelectedItem(adminComboBox);
+
+            if (choice != null && !choice.Equals(defultChoice))
+            {
+                Admin admin = data.Admins.Find(a => a.Name.Equals(choice));
+                List<Ticket> tmp = new List<Ticket>();
+                tickets = FindAdminAnsweredTicket(admin);
+            }
+        }
+
+        /// <summary>
+        /// Admin office filter
+        /// </summary>
+        private void AdminOfficeFilter()
+        {
+            string choice = GetSelectedItem(officeAdminComboBox);
+
+            if (choice != null && !choice.Equals(defultChoice))
+            {
+                Admin admin = data.Admins.Find(a => a.Office.Equals(choice));
+                List<Ticket> tmp = new List<Ticket>();
+                tickets = FindAdminAnsweredTicket(admin);
+            }
+        }
+
+        /// <summary>
+        /// Find ticket that is answered by admin
+        /// </summary>
+        /// <param name="admin"></param>
+        /// <returns></returns>
+        private List<Ticket> FindAdminAnsweredTicket(Admin admin)
+        {
+            List<Ticket> tmp = new List<Ticket>();
+            foreach (Ticket t in tickets)
+            {
+                foreach (KeyValuePair<int, Answer> entry in t.Answers)
+                {
+                    if (entry.Key == admin.ID)
+                    {
+                        tmp.Add(t);
+                        break;
+                    }
+                }
+            }
+            return tickets = tmp;
+        }
+    }
+}
